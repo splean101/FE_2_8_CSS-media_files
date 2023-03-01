89ac8b47fe626598d7008973146c42ca01db5444
splean101 <101splean@gmail.com>
2023-03-01 12:40:49 +0200

    done

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,29 +1,35 @@
-import React from "react";
-import styles from './Products.module.scss'
-//import iphone from '../../assets/iphone.jpg'
+import React from 'react';
+import styles from './Products.module.scss';
+import iphone from '../../assets/iphone.jpg';
+import watch from '../../assets/watch.jpg';
+import nofoto from '../../assets/no-foto.jpg';
 
-function Product(props){
+function Product(props) {
+  const remove = () => {
+    props.onRemove(props.id);
+  };
+let imgSrc = nofoto;
+if(props.name === 'iphone' || props.name === 'Iphone'){
+    imgSrc = iphone;
+} else if (props.name === 'watch' || props.name === 'Watch'){
+    imgSrc = watch;
+}
 
-    const remove = () => {
-        props.onRemove(props.id)
-    }
+  return (
+    <div className={styles.product}>
+      <div className="img">
+        <img src={imgSrc} />
+      </div>
 
-    return (
-        <div className={styles.product}>
-            <div className="body">
-                <h1 className="title">{props.name}</h1>
-                <div className="price">{props.price}</div>
-            </div>
-            <button onClick={remove} type="button">Remove</button>
-        </div>
-    )
+      <div className="body">
+        <h1 className="title">{props.name}</h1>
+        <div className="price">{props.price}</div>
+      </div>
+      <button onClick={remove} type="button">
+        Remove
+      </button>
+    </div>
+  );
 }
 
 export default Product;
-
-// const nameChecker = (str) => {
-//     if (str.length <= 1 || str.trim() === '') {
-//       return false;
-//     }
-//     return true;
-//   };
